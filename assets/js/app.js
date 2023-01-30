af8ca778fa15832e50c0cf217635daf8008ea93f
France900 <francesco.em.livio@gmail.com>
2023-01-30 09:50:33 +0100

    implementazioni multiple
    aggiunto vari elementi al progetto in un unica sessione di lavoro

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,107 @@
   \***********************/
 /***/ (() => {
 
+// carica documento html prima
+$(document).ready(function () {
+  console.log("test link succesfull");
 
+  // funzione + per aggiungere elementi
+  $('.pluss').click(function () {
+    var refertChoice = prompt("Scegliere fra il referto 1, 2, 3, 4 (es. referto_2)");
+    if (refertChoice == "referto_1") {
+      var slider = '';
+      for (var i = 0; i < referto_1.length; i++) {
+        slider += '<div class="mySlides fade">' + '<div class="numbertext">' + (i + 1) + '/' + (referto_1.length - 1) + '</div>' + '<img src="' + referto_1[i] + '"' + '</div>';
+      }
+      $('main').append(slider);
+    } else if (refertChoice == "referto_2") {
+      var slider = '';
+      for (var _i = 0; _i < referto_2.length; _i++) {
+        slider += '<div class="mySlides fade">' + '<div class="numbertext">' + (_i + 1) + '/' + (referto_2.length - 1) + '</div>' + '<img src="' + referto_2[_i] + '"' + '</div>';
+      }
+      $('main').append(slider);
+    } else if (refertChoice == "referto_3") {
+      var slider = '';
+      for (var _i2 = 0; _i2 < referto_3.length; _i2++) {
+        slider += '<div class="mySlides fade">' + '<div class="numbertext">' + (_i2 + 1) + '/' + (referto_3.length - 1) + '</div>' + '<img src="' + referto_3[_i2] + '"' + '</div>';
+      }
+      $('main').append(slider);
+    } else if (refertChoice == "referto_4") {
+      var slider = '';
+      for (var _i3 = 0; _i3 < referto_4.length; _i3++) {
+        slider += '<div class="mySlides fade">' + '<div class="numbertext">' + (_i3 + 1) + '/' + (referto_4.length - 1) + '</div>' + '<img src="' + referto_4[_i3] + '"' + '</div>';
+      }
+      $('main').append(slider);
+    } else {
+      alert("Il valore inserito non è corretto, si prega di riprovare inserendo il valore corretto (es. referto 2)");
+    }
+  });
+
+  //funzione - per cancellare elementi
+  $('.minus').click(function () {
+    var elements = $('main .element');
+    if (elements.length > 0) {
+      elements.last().remove();
+    }
+  });
+
+  //#region archivio immagini
+  var referto_1 = ['assets/img/carosello1/img1.jpg', 'assets/img/carosello1/img2.jpg', 'assets/img/carosello1/img3.jpg'];
+  var referto_2 = ['assets/img/carosello2/img4.jpg', 'assets/img/carosello2/img5.jpg', 'assets/img/carosello2/img6.jpg', 'assets/img/carosello2/img7.jpg', 'assets/img/carosello2/img8.jpg'];
+  var referto_3 = ['assets/img/carosello3/img9.jpg', 'assets/img/carosello3/img10.jpg', 'assets/img/carosello3/img11.jpg', 'assets/img/carosello3/img12.jpg'];
+  var referto_4 = ['assets/img/carosello4/img13.jpg', 'assets/img/carosello4/img14.jpg', 'assets/img/carosello4/img15.jpg', 'assets/img/carosello4/img16.jpg', 'assets/img/carosello4/img17.jpg', 'assets/img/carosello4/img18.jpg'];
+  //#endregion
+});
+
+/*
+$('.pluss').click(function() {
+  var refertChoice = prompt("Scegliere fra il referto 1, 2, 3, 4 (es. referto_2)");
+  console.log(refertChoice[3]);
+  if (
+    refertChoice == "referto_1" || 
+    refertChoice == "referto_2" || 
+    refertChoice == "referto_3" || 
+    refertChoice == "referto_4"
+    ) {
+      var slider = ''
+      for (let i = 0; i < refertChoice.length; i++) {
+        slider +=
+        '<div class="mySlides fade">' +
+          '<div class="numbertext">' + (i+1) + '/' + (refertChoice.length-1) + '</div>' +
+          '<img src="'+ refertChoice[i] +'" style="width:100%">' +
+        '</div>';
+        console.log(refertChoice[i]);
+      }
+      $('main').append(slider);
+  } else {
+    alert("Il valore inserito non è corretto, si prega di riprovare inserendo il valore corretto (es. referto_2)")
+  }  
+});
+*/
+
+// funzione + per aggiungere elementi
+// $('.pluss').click(function() {
+//   var refertChoice = prompt("Scegliere fra il referto 1, 2, 3, 4 (es. referto_2)");
+//   if (refertChoice == "referto_1") {
+//     $('main').append(
+//       '<div class="element1">Element1</div>'
+//     );
+//   } else if (refertChoice == "referto_2") {
+//     $('main').append(
+//       '<div class="element2">Element2</div>'
+//     );
+//   } else if (refertChoice == "referto_3") {
+//     $('main').append(
+//       '<div class="element3">Element3</div>'
+//     );
+//   } else if (refertChoice == "referto_4") {
+//     $('main').append(
+//       '<div class="element4">Element4</div>'
+//     );
+//   } else {
+//     alert("Il valore inserito non è corretto, si prega di riprovare inserendo il valore corretto (es. referto 2)")
+//   }  
+// });
 
 /***/ }),
 
