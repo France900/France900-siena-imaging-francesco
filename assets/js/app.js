11739bdd88b7eaf39ce395cf7e5ba77d5dde1915
France900 <74263241+France900@users.noreply.github.com>
2023-02-02 17:25:42 +0100

    aggiornamento immagini tramite la variabile definita dal prompt
    presenta due bug riportati in fondo alla pagina
    
    e manca tutto il readme e la documentazione

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,16 +9,13 @@
 
 //#region archivio immagini
 var referto = {
-  carlo: ['assets/img/carosello1/img1.jpg', 'assets/img/carosello1/img2.jpg', 'assets/img/carosello1/img3.jpg'],
-  marco: ['assets/img/carosello2/img4.jpg', 'assets/img/carosello2/img5.jpg', 'assets/img/carosello2/img6.jpg', 'assets/img/carosello2/img7.jpg', 'assets/img/carosello2/img8.jpg'],
+  mario: ['assets/img/carosello1/img1.jpg', 'assets/img/carosello1/img2.jpg', 'assets/img/carosello1/img3.jpg'],
+  pippo: ['assets/img/carosello2/img4.jpg', 'assets/img/carosello2/img5.jpg', 'assets/img/carosello2/img6.jpg', 'assets/img/carosello2/img7.jpg', 'assets/img/carosello2/img8.jpg'],
   gino: ['assets/img/carosello3/img9.jpg', 'assets/img/carosello3/img10.jpg', 'assets/img/carosello3/img11.jpg', 'assets/img/carosello3/img12.jpg'],
   pino: ['assets/img/carosello4/img13.jpg', 'assets/img/carosello4/img14.jpg', 'assets/img/carosello4/img15.jpg', 'assets/img/carosello4/img16.jpg', 'assets/img/carosello4/img17.jpg', 'assets/img/carosello4/img18.jpg']
 };
 // array nomi referti
 var nomiReferti = Object.keys(referto);
-
-// database immagini
-var arrayNest = ["img/img_lights_wide.jpg", "img/img_nature_wide.jpg", "img/img_snow_wide.jpg", "img/img_lights_wide.jpg", "img/img_nature_wide.jpg", "img/img_snow_wide.jpg", "img/img_lights_wide.jpg", "img/img_nature_wide.jpg", "img/img_snow_wide.jpg", "img/img_lights_wide.jpg", "img/img_nature_wide.jpg", "img/img_snow_wide.jpg", "img/img_lights_wide.jpg", "img/img_nature_wide.jpg", "img/img_snow_wide.jpg", "img/img_mountains_wide.jpg"];
 //#endregion
 
 // definisco l'indice di controllo per le slide
@@ -26,9 +23,10 @@ var currentSlide = 1;
 
 // richiamo le slide e collegato html
 $('.pluss').click(function () {
+  choseReferto = prompt("Sono disponibili i referti di: " + nomiReferti + ". Digitare il nome interessato in minuscolo (es. giacomo)");
   inserisciCarosello();
 });
-inserisciCarosello();
+//inserisciCarosello()
 
 //funzione - per cancellare elementi
 $('.minus').click(function () {
@@ -40,12 +38,12 @@ $('.minus').click(function () {
 
 // funzione per l'inserimento del carosello in modo modulare
 function inserisciCarosello() {
-  var section = '<section>' + '<div class="slideshow-container mySlides fade">' + '</div>' + '<div class="slideControls">' + '<div class="prev" onclick="sliderMover(\'retreat\')">&#10094;</div>' + '<div class="dots" style="text-align:center">' + '</div>' + '<div class="next" onclick="sliderMover(\'advance\')">&#10095;</i></div>' + '</div>' + '</section>';
+  var section = '<section class="' + choseReferto + '">' + '<div class="mySlides fade">' + '</div>' + '<div class="slideControls">' + '<div class="prev" onclick="sliderMover(\'retreat\')">&#10094;</div>' + '<div class="dots" style="text-align:center">' + '</div>' + '<div class="next" onclick="sliderMover(\'advance\')">&#10095;</i></div>' + '</div>' + '</section>';
   // QUI IL TAG PRINCIPALE IN CUI SI APPENDE - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   $('main').append(section);
   aggiornaImmagine();
-  // genero i dot in numero uguale a arrayNest.lenght
-  for (var index = 0; index < arrayNest.length; index++) {
+  // genero i dot in numero uguale alla lunghezza dell'array desiderato
+  for (var index = 0; index < referto.mario.length; index++) {
     var dotsDeploy = '<span class="dot" onclick="dotSelect(' + index + ')"></span>';
     $('.dots').append(dotsDeploy);
   }
@@ -58,7 +56,7 @@ function inserisciCarosello() {
 function sliderMover(direction) {
   // va messo fuori: var currentSlide = 1;
   if (direction == 'advance') {
-    if (currentSlide == arrayNest.length) {
+    if (currentSlide == referto.mario.length) {
       currentSlide = 1;
     } else {
       currentSlide = currentSlide + 1;
@@ -67,7 +65,7 @@ function sliderMover(direction) {
     aggiornaDot();
   } else if (direction == 'retreat') {
     if (currentSlide == 1) {
-      currentSlide = arrayNest.length;
+      currentSlide = referto.mario.length;
     } else {
       currentSlide = currentSlide - 1;
     }
@@ -86,18 +84,21 @@ function dotSelect(parametro) {
 // creazione e l'aggiornamento delle immagini nell'html
 function aggiornaImmagine() {
   $('.mySlides').empty();
-  var element = '<div class="numbertext">' + currentSlide + '/' + arrayNest.length + '</div>' + '<img src="' + arrayNest[currentSlide - 1] + '">';
+  var element = '<div class="numbertext">' + currentSlide + '/' + referto.mario.length + '</div>' + '<img src="' + referto.mario[currentSlide - 1] + '">';
   $('.mySlides').append(element);
 }
 
 // aggiungo il colore del dot selezionato
 function aggiornaDot() {
-  for (var index = 0; index < arrayNest.length; index++) {
+  for (var index = 0; index < referto.mario.length; index++) {
     dots[index].classList.remove("active");
   }
   var dotPoition = currentSlide - 1;
   dots[dotPoition].classList.add("active");
 }
+
+// +'.'+choseReferto   da aggiungere a section con un id per avere una section univoca di accesso e quindi cambiare i selettori seguenti con ad esempio: $("tutti i dot figli di section con id 'choseReferto'")
+// sostituendo choseReferto a mario non prende il risultato del prompt, che è mario o pippo)
 
 /***/ }),
 
--- src/js/appBackup.js
+++ src/js/appBackup.js
@@ -1,12 +1,27 @@
 //#region archivio immagini
-var referto = {
+var /*referto*/arrayNest = {
   mario: ['assets/img/carosello1/img1.jpg', 'assets/img/carosello1/img2.jpg', 'assets/img/carosello1/img3.jpg'],
   pippo: ['assets/img/carosello2/img4.jpg', 'assets/img/carosello2/img5.jpg', 'assets/img/carosello2/img6.jpg', 'assets/img/carosello2/img7.jpg', 'assets/img/carosello2/img8.jpg'],
   gino: ['assets/img/carosello3/img9.jpg', 'assets/img/carosello3/img10.jpg', 'assets/img/carosello3/img11.jpg', 'assets/img/carosello3/img12.jpg'],
   pino: ['assets/img/carosello4/img13.jpg', 'assets/img/carosello4/img14.jpg', 'assets/img/carosello4/img15.jpg', 'assets/img/carosello4/img16.jpg', 'assets/img/carosello4/img17.jpg', 'assets/img/carosello4/img18.jpg'],
 }
 // array nomi referti
-var nomiReferti = Object.keys(referto);
+var nomiReferti = Object.keys(/*referto*/arrayNest);
+
+// database immagini
+var arrayNest_sospeso = [
+  /*
+  "img/img_lights_wide.jpg",
+  "img/img_nature_wide.jpg",
+  "img/img_snow_wide.jpg",
+  "img/img_mountains_wide.jpg"
+  */
+  'assets/img/carosello2/img4.jpg',
+  'assets/img/carosello2/img5.jpg',
+  'assets/img/carosello2/img6.jpg',
+  'assets/img/carosello2/img7.jpg',
+  'assets/img/carosello2/img8.jpg'
+]
 //#endregion
 
 // definisco l'indice di controllo per le slide
@@ -15,8 +30,8 @@ var currentSlide = 1;
 
 
 // richiamo le slide e collegato html
+//var choseReferto = prompt("Sono disponibili i referti di: "+nomiReferti+". Digitare il nome interessato in minuscolo (es. giacomo)")
 $('.pluss').click(function() {
-  choseReferto = prompt("Sono disponibili i referti di: "+nomiReferti+". Digitare il nome interessato in minuscolo (es. giacomo)")
   inserisciCarosello()
 })
 //inserisciCarosello()
@@ -33,12 +48,13 @@ $('.minus').click(function() {
 // funzione per l'inserimento del carosello in modo modulare
 function inserisciCarosello() {
   var section = 
-  '<section class="'+choseReferto+'">'+
+  '<section>'+
   '<div class="mySlides fade">'+
   '</div>'+
   '<div class="slideControls">'+
   '<div class="prev" onclick="sliderMover(\'retreat\')">&#10094;</div>'+
-  '<div class="dots" style="text-align:center">'+'</div>'+
+  '<div class="dots" style="text-align:center">'+
+  '</div>'+
   '<div class="next" onclick="sliderMover(\'advance\')">&#10095;</i></div>'+
   '</div>'+
   '</section>'
@@ -46,7 +62,7 @@ function inserisciCarosello() {
   $('main').append(section);
   aggiornaImmagine()
   // genero i dot in numero uguale alla lunghezza dell'array desiderato
-  for (let index = 0; index < referto.mario.length; index++) {
+  for (let index = 0; index < arrayNest.mario.length; index++) {
     var dotsDeploy = 
     '<span class="dot" onclick="dotSelect('+index+')"></span>'
     ;
@@ -62,7 +78,7 @@ function inserisciCarosello() {
 function sliderMover(direction) {
   // va messo fuori: var currentSlide = 1;
   if (direction == 'advance') {
-    if (currentSlide==referto.mario.length) {
+    if (currentSlide==arrayNest.mario.length) {
       currentSlide = 1
     } else {
       currentSlide =currentSlide +1
@@ -71,7 +87,7 @@ function sliderMover(direction) {
     aggiornaDot()
   } else if (direction == 'retreat') {
     if (currentSlide==1) {
-      currentSlide=referto.mario.length
+      currentSlide=arrayNest.mario.length
     } else {
       currentSlide=currentSlide -1
     }
@@ -92,20 +108,16 @@ function dotSelect(parametro) {
 function aggiornaImmagine() {
   $('.mySlides').empty();
   var element = 
-  '<div class="numbertext">'+ currentSlide +'/'+ referto.mario.length +'</div>'+
-  '<img src="'+ referto.mario[currentSlide-1] +'">'
+  '<div class="numbertext">'+ currentSlide +'/'+ arrayNest.mario.length +'</div>'+
+  '<img src="'+ arrayNest.mario[currentSlide-1] +'">'
   $('.mySlides').append(element);
 }
 
 // aggiungo il colore del dot selezionato
 function aggiornaDot() {
-  for (let index = 0; index < referto.mario.length; index++) {
+  for (let index = 0; index < arrayNest.mario.length; index++) {
     dots[index].classList.remove("active")
   }
   var dotPoition = currentSlide - 1
   dots[dotPoition].classList.add("active");
 }
-
-
-// +'.'+choseReferto   da aggiungere a section con un id per avere una section univoca di accesso e quindi cambiare i selettori seguenti con ad esempio: $("tutti i dot figli di section con id 'choseReferto'")
-// sostituendo choseReferto a mario non prende il risultato del prompt, che è mario o pippo)
